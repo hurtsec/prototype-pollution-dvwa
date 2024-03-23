2271cca9d363bd389dd36d72f2d0facfe005daa8
J Hurt <j@justinhurt.com>
2024-03-23 16:18:29 -0500

    set up state mangagement for user object

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <section className="logo">
-          <h1>Prototype Pollution DVWA</h1>
+  const MainComponent = () => {
+    const [user, setUser] = useState()
+    
+    useEffect(() => {
+      axios.get('http://localhost:5001/user')
+        .then(res => setUser(res.data))
+        .catch(error => console.error(error))
+    }, [])
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <section className="logo">
+            <h1>Prototype Pollution DVWA</h1>
+          </section>
+          <nav className="menu">
+            <a href="#">Home</a>
+            <a href="#">Settings</a>
+          </nav>
+        </header>
+        <section className="app-body">
+          {/* Information disclosure occurs when attacker pollutes the JavaScript Object Prototype
+              with an `admin` property, triggering this conditional element to be revealed. */}
+          {(user && user.admin) && <p class="flag">FLAG:f10c4$7c1i3n7pr0707yp3p011u7i0n</p>}
         </section>
-        <nav className="menu">
-          <a href="#">Home</a>
-          <a href="#">Settings</a>
-        </nav>
-      </header>
-      <section className="app-body">
-        {/* Information disclosure occurs when attacker pollutes the JavaScript Object Prototype
-            with an `admin` property, triggering this conditional element to be revealed. */}
-        {(user && user.admin) && <p class="flag">FLAG:f10c4$7c1i3n7pr0707yp3p011u7i0n</p>}
-      </section>
-    </div>
-  );
+      </div>
+    );
+  }
+
+  return (<MainComponent />)
 }
 
 export default App;
