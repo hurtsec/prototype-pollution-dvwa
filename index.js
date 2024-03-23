4dc1ceee8f1817928b39c37f7f130eb7cea5e161
J Hurt <hurtsec@Js-MacBook-Pro.local>
2024-03-23 16:00:29 -0500

    create mocked user object retrieval endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const uuid = require(uuid)
 
 const app = express()
 const port = 5001
@@ -9,6 +10,17 @@ app.use(express.json())
 
 app.get('/', async(req, res) => {
     res.send('Hello, world!')
+})
+
+app.get('/user', (req, res) => {
+    // Mocking in a user object that would typically be
+    // retrieved from the DB
+    res.json({
+        id: uuid.v4(),
+        userSettings: {
+            dark: true
+        }
+    })
 })
 
 app.listen(port, () => {
