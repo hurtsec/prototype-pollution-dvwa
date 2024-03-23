b46e4f6e084987802d3f70d8ae9478de084eae48
J Hurt <hurtsec@Js-MacBook-Pro.local>
2024-03-23 15:18:12 -0500

    setup middleware

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const port = 5001
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async(req, res) => {
     res.send('Hello, world!')
