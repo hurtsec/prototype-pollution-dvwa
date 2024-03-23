9968bdcdf5f8e94dcd38f41491a9e7ba1fe56a11
J Hurt <j@justinhurt.com>
2024-03-23 16:10:45 -0500

    create jsx html skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="logo">
+          <h1>Prototype Pollution DVWA</h1>
+        </section>
+        <nav className="menu">
+          <a href="#">Home</a>
+          <a href="#">Settings</a>
+        </nav>
       </header>
+      <section className="app-body">
+        {/* Information disclosure occurs when attacker pollutes the JavaScript Object Prototype
+            with an `admin` property, triggering this conditional element to be revealed. */}
+        {(user && user.admin) && <p class="flag">FLAG:f10c4$7c1i3n7pr0707yp3p011u7i0n</p>}
+      </section>
     </div>
   );
 }
