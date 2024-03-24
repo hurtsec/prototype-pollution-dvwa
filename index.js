84327d52843fc7e323df8d5e66fd75885f9f3182
J Hurt <j@justinhurt.com>
2024-03-24 13:45:07 -0500

    install lodash, create vulnerable endpoint, create endpoint to be exploited for information disclosure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const uuid = require('uuid')
+const _ = require('lodash')
 
 const app = express()
 const port = 5001
@@ -21,6 +22,38 @@ app.get('/user', (req, res) => {
             dark: true
         }
     })
+})
+
+app.post('/settings', (req, res) => {
+    const userSettings = req.body.userSettings
+    // Mocking in a user object that would typically be
+    // retrieved from the DB
+    const user = {
+        id: uuid.v4(),
+        userSettings: {
+            dark: true
+        }
+    }
+
+    const updatedSettings = _.merge(user.userSettings, userSettings)
+    res.status(200).send('Your settings wer successfully updated')
+})
+
+app.get('/admin', (req, res) => {
+    // Mocking in a user object that would typically be
+    // retrieved from the DB
+    const user = {
+        id: uuid.v4(),
+        userSettings: {
+            dark: true
+        }
+    }
+
+    if (user.admin) {
+        res.status(200).send('FLAG:f10c4$7$3rv3rpr0707yp3p011u73d')
+    } else {
+        res.sendStatus(403)
+    }
 })
 
 app.listen(port, () => {
