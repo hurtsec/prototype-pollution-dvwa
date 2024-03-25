e9336d97d6323f5ff3c27fab310f93c80cf83e62
J Hurt <j@justinhurt.com>
2024-03-25 01:22:56 -0500

    styling update

--- client/src/App.js
+++ client/src/App.js
@@ -25,19 +25,22 @@ function App() {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <section className="logo">
-            <h1>Prototype Pollution DVWA</h1>
+        <section className="app-main">
+          <header className="App-header">
+            <section className="logo">
+              <h1>Prototype Pollution DVWA</h1>
+            </section>
+            <nav className="menu">
+              <a href="#">Home</a>
+              <a href="#">Settings</a>
+            </nav>
+          </header>
+          <section className="app-body">
+            {/* Information disclosure occurs when attacker pollutes the JavaScript Object Prototype
+                with an `admin` property, triggering this conditional element to be revealed. */}
+            {(user && user.admin) && <p className="flag">FLAG:f10c4$7c1i3n7pr0707yp3p011u7i0n</p>}
+            {(user && !user.admin) && <p>Application currently under development. Only admin access is permitted.</p>}
           </section>
-          <nav className="menu">
-            <a href="#">Home</a>
-            <a href="#">Settings</a>
-          </nav>
-        </header>
-        <section className="app-body">
-          {/* Information disclosure occurs when attacker pollutes the JavaScript Object Prototype
-              with an `admin` property, triggering this conditional element to be revealed. */}
-          {(user && user.admin) && <p className="flag">FLAG:f10c4$7c1i3n7pr0707yp3p011u7i0n</p>}
         </section>
       </div>
     );
